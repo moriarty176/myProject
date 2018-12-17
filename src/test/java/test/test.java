d74cf1268cc6260333afb668d2da844f89322b83
jamesmoriarty176 <jamesmoriarty176@gmail.com>
2018-12-17 17:47:11 -0600

    Initial commit

---
+++ src/test/java/test/test.java
@@ -0,0 +1,19 @@
+package test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver","/Users/talgatabykeev/Documents/selenium dependencies/drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/ login.aspx");
+
+
+    }
+
+}
